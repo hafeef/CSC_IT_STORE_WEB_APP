8d8bb0c0a9bf73129b873261f3bbfa7920dc41a8
SHAIK HAFEEF <hafeefshaik@gmail.com>
2017-05-10 18:01:51 +0530

    Published database changes to azure.

--- CSC.IT.Store.ViewModels/Identity/ChangePasswordViewModel.cs
+++ CSC.IT.Store.ViewModels/Identity/ChangePasswordViewModel.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CSC.IT.Store.ViewModels.Identity
 {
     public class ChangePasswordViewModel
     {
-        [Required, Display(Name ="Old Password"), DataType(DataType.Password)]
+        [Required, Display(Name = "Old Password"), DataType(DataType.Password)]
         public string OldPassword { get; set; }
 
         [Required, Display(Name = "New Password"), DataType(DataType.Password)]
